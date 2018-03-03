ea31a87d0ff4b806d892bb1a8b722ae0191d1122
Felix Kollmann <felix.kollmann@twinpoint.de>
2018-03-03 19:39:29 +0100

    Fixed git input was failing if no repository was found

--- ver/git.go
+++ ver/git.go
@@ -2,6 +2,9 @@ package ver
 
 import (
 	"os/exec"
+	"syscall"
+
+	log "github.com/Sirupsen/logrus"
 )
 
 func TryReadFromGit() (*VersionInformation, error) {
@@ -9,6 +12,17 @@ func TryReadFromGit() (*VersionInformation, error) {
 	rev, err := exec.Command("git", "log", "-n", "1", "--pretty=format:%H").Output()
 
 	if err != nil {
+		// not a git repository?
+		if exiterr, ok := err.(*exec.ExitError); ok {
+			if status, ok := exiterr.Sys().(syscall.WaitStatus); ok {
+				if status.ExitStatus() == 128 /* not found */ {
+					log.Debugf("Not a Git repository; no revision retrieved")
+
+					return nil, nil
+				}
+			}
+		}
+
 		return nil, err
 	}
 
@@ -16,6 +30,8 @@ func TryReadFromGit() (*VersionInformation, error) {
 	vi := MakeVersionInformation()
 
 	vi.Revision = string(rev)
+
+	log.Debugf("Found Git revision: %v", vi.Revision)
 
 	return vi, nil
 }
---
+++ ver/git_test.go
@@ -0,0 +1,41 @@
+package ver
+
+import (
+	"os"
+	"testing"
+
+	log "github.com/Sirupsen/logrus"
+)
+
+func TestRunGitHere(t *testing.T) {
+	log.SetLevel(log.DebugLevel)
+
+	// try read revision
+	vi, err := TryReadFromGit()
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	t.Logf("Revision: %v", vi.Revision)
+}
+
+func TestRunGitNoRepository(t *testing.T) {
+	log.SetLevel(log.DebugLevel)
+
+	// change to test data dir
+	currDir, _ := os.Getwd()
+	os.Chdir(os.TempDir())
+	defer os.Chdir(currDir)
+
+	// try read revision
+	vi, err := TryReadFromGit()
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if vi != nil {
+		t.Fatal("No version information was expected")
+	}
+}
